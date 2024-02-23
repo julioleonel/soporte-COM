2671fb91d6e45b9f209f7b36712970a4f869d6c1
“julioleonel” <jsendra@fie.undef.edu.ar>
2024-02-23 20:03:15 -0300

    Agregada entidad Usuario

---
+++ src/main/java/ar/edu/fie/undef/soporteCOM/domain.entities/Usuario.java
@@ -0,0 +1,83 @@
+package ar.edu.fie.undef.soporteCOM.domain.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String grado;
+    private String nombre;
+    private String apellido;
+    private String username;
+    private String password;
+    private String email;
+
+    //Constructor vacio
+    public Usuario() {
+    }
+
+
+    //Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGrado() {
+        return grado;
+    }
+
+    public void setGrado(String grado) {
+        this.grado = grado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
